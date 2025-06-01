51a62eefaa281305decce425032c107d7d753e92
Farouk Adeleke <fadeleke57@gmail.com>
2025-05-31 20:44:45 -0400

    Remove boilerplate client

--- api/index.ts
+++ api/index.ts
@@ -1,42 +1,47 @@
-import {TodoMCP} from "./TodoMCP.ts";
-import {getStytchOAuthEndpointUrl, stytchBearerTokenAuthMiddleware} from "./lib/auth";
-import {TodoAPI} from "./TodoAPI.ts";
-import {cors} from "hono/cors";
-import {Hono} from "hono";
+import { MemoryMCP } from "./MemoryMCP.ts";
+import {
+  getStytchOAuthEndpointUrl,
+  stytchBearerTokenAuthMiddleware,
+} from "./lib/auth";
+import { MemoryAPI } from "./MemoryAPI.ts";
+import { cors } from "hono/cors";
+import { Hono } from "hono";
 
-// Export the TodoMCP class so the Worker runtime can find it
-export {TodoMCP};
+// Export the MCP class so the Worker runtime can find it
+export { MemoryMCP };
 
 export default new Hono<{ Bindings: Env }>()
-    .use(cors())
+  .use(cors())
 
-    // Mount the TODO API underneath us
-    .route('/api', TodoAPI)
+  // Mount the MEMORY API underneath us
+  .route("/api", MemoryAPI)
 
-    // Serve the OAuth Authorization Server response for Dynamic Client Registration
-    .get('/.well-known/oauth-authorization-server', async (c) => {
-        const url = new URL(c.req.url);
-        return c.json({
-            issuer: c.env.STYTCH_PROJECT_ID,
-            // Link to the OAuth Authorization screen implemented within the React UI
-            authorization_endpoint: `${url.origin}/oauth/authorize`,
-            token_endpoint: getStytchOAuthEndpointUrl(c.env, 'oauth2/token'),
-            registration_endpoint: getStytchOAuthEndpointUrl(c.env, 'oauth2/register'),
-            scopes_supported: ['openid', 'profile', 'email', 'offline_access'],
-            response_types_supported: ['code'],
-            response_modes_supported: ['query'],
-            grant_types_supported: ['authorization_code', 'refresh_token'],
-            token_endpoint_auth_methods_supported: ['none'],
-            code_challenge_methods_supported: ['S256'],
-        })
-    })
+  // Serve the OAuth Authorization Server response for Dynamic Client Registration
+  .get("/.well-known/oauth-authorization-server", async (c) => {
+    const url = new URL(c.req.url);
+    return c.json({
+      issuer: c.env.STYTCH_PROJECT_ID,
+      authorization_endpoint: `${c.env.CLIENT_URL}/oauth/authorize`, // Link to the OAuth Authorization screen implemented within the Next.JS UI
+      token_endpoint: getStytchOAuthEndpointUrl(c.env, "oauth2/token"),
+      registration_endpoint: getStytchOAuthEndpointUrl(
+        c.env,
+        "oauth2/register"
+      ),
+      scopes_supported: ["openid", "profile", "email", "offline_access"],
+      response_types_supported: ["code"],
+      response_modes_supported: ["query"],
+      grant_types_supported: ["authorization_code", "refresh_token"],
+      token_endpoint_auth_methods_supported: ["none"],
+      code_challenge_methods_supported: ["S256"],
+    });
+  })
 
-    // Let the MCP Server have a go at handling the request
-    .use('/sse/*', stytchBearerTokenAuthMiddleware)
-    .route('/sse', new Hono().mount('/', TodoMCP.serveSSE('/sse').fetch))
+  // Let the MCP Server have a go at handling the request
+  .use("/sse/*", stytchBearerTokenAuthMiddleware)
+  .route("/sse", new Hono().mount("/", MemoryMCP.serveSSE("/sse").fetch))
 
-    .use('/mcp', stytchBearerTokenAuthMiddleware)
-    .route('/mcp', new Hono().mount('/', TodoMCP.serve('/mcp').fetch))
+  .use("/mcp", stytchBearerTokenAuthMiddleware)
+  .route("/mcp", new Hono().mount("/", MemoryMCP.serve("/mcp").fetch))
 
-    // Finally - serve static assets from Vite
-    .mount('/', (req, env) => env.ASSETS.fetch(req))
+  // Finally - serve static assets from Vite
+  .mount("/", (req, env) => env.ASSETS.fetch(req));
--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -5,6 +5,7 @@ declare namespace Cloudflare {
     MEMORY: KVNamespace;
     TODOS: KVNamespace;
     STYTCH_PROJECT_ID: "project-live-ebb7735e-eebd-465b-966d-046dd8475296";
+    CLIENT_URL: "https://memory.spydr.dev";
     MCP_OBJECT: DurableObjectNamespace<import("./api/index").TodoMCP>;
     ASSETS: Fetcher;
   }
